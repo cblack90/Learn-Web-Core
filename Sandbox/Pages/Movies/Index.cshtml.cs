c68202da37d235c327bc8d87595972b4ec826981
cblack90 <46687746+cblack90@users.noreply.github.com>
2019-10-12 16:04:22 -0600

    working on getting search functionality to work.

--- Sandbox/Pages/Movies/Index.cshtml.cs
+++ Sandbox/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Sandbox.Data;
 using Sandbox.Models;
@@ -19,10 +20,21 @@ namespace Sandbox.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
             Movie = await _context.Movie.ToListAsync();
         }
     }
