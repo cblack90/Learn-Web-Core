6baa9218d94b76785318e1ea6e98cb2f468e23cc
cblack90 <46687746+cblack90@users.noreply.github.com>
2019-10-12 17:13:26 -0600

    set up filter by query string and drop down menu.

--- Sandbox/Pages/Movies/Index.cshtml.cs
+++ Sandbox/Pages/Movies/Index.cshtml.cs
@@ -27,15 +27,35 @@ namespace Sandbox.Pages.Movies
         [BindProperty(SupportsGet = true)]
         public string MovieGenre { get; set; }
 
+        //public async Task OnGetAsync()
+        //{
+        //    var movies = from m in _context.Movie
+        //                 select m;
+        //    if (!string.IsNullOrEmpty(SearchString))
+        //    {
+        //        movies = movies.Where(s => s.Title.Contains(SearchString));
+        //    }
+        //    Movie = await _context.Movie.ToListAsync();
+        //}
         public async Task OnGetAsync()
         {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
             var movies = from m in _context.Movie
                          select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 movies = movies.Where(s => s.Title.Contains(SearchString));
             }
-            Movie = await _context.Movie.ToListAsync();
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
